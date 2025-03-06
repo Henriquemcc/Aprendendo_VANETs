de85fe001c7cd0f6772907ff7f96af0a351ad422
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-03-06 12:06:14 -0300

    Adicionando exemplos de simulação do veins_inet

---
+++ 02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/VANETTutorials/out/clang-release/src/veins_inet/VeinsInetTransparentMobility.o.d
@@ -0,0 +1,176 @@
+../out/clang-release/src/veins_inet/VeinsInetTransparentMobility.o: \
+  veins_inet/VeinsInetTransparentMobility.cc \
+  veins_inet/VeinsInetTransparentMobility.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/mobility/base/MobilityBase.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/ModuleAccess.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/INETDefs.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/precompiled_release.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/precompiled.h \
+  /omnetpp-6.0/include/omnetpp.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/opp_defines.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/features.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/Compat.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/InitStages.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/InitStageRegistry.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/StringFormat.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/geometry/common/CanvasProjection.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/geometry/common/RotationMatrix.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/geometry/common/Coord.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/INETMath.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/geometry/common/EulerAngles.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/Units.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/geometry/common/Quaternion.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/mobility/contract/IMobility.h \
+  /home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/ModuleRefByPar.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIMobility.h \
+  ../../veins/src/veins/base/modules/BaseMobility.h \
+  ../../veins/src/veins/veins.h \
+  ../../veins/src/veins/base/modules/BatteryAccess.h \
+  ../../veins/src/veins/base/modules/BaseModule.h \
+  ../../veins/src/veins/base/utils/HostState.h \
+  ../../veins/src/veins/base/modules/BaseBattery.h \
+  ../../veins/src/veins/base/utils/Coord.h \
+  ../../veins/src/veins/base/utils/FWMath.h \
+  ../../veins/src/veins/base/utils/Move.h \
+  ../../veins/src/veins/base/modules/BaseWorldUtility.h \
+  ../../veins/src/veins/base/utils/FindModule.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIScenarioManager.h \
+  ../../veins/src/veins/base/connectionManager/BaseConnectionManager.h \
+  ../../veins/src/veins/base/utils/AntennaPosition.h \
+  ../../veins/src/veins/base/connectionManager/NicEntry.h \
+  ../../veins/src/veins/base/utils/Heading.h \
+  ../../veins/src/veins/modules/utility/HasLogProxy.h \
+  ../../veins/src/veins/modules/obstacle/ObstacleControl.h \
+  ../../veins/src/veins/modules/obstacle/Obstacle.h \
+  ../../veins/src/veins/modules/world/annotations/AnnotationManager.h \
+  ../../veins/src/veins/modules/utility/BBoxLookup.h \
+  ../../veins/src/veins/modules/obstacle/VehicleObstacleControl.h \
+  ../../veins/src/veins/modules/obstacle/MobileHostObstacle.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIBuffer.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIConstants.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIColor.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIConnection.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCICoord.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h \
+  ../../veins/src/veins/modules/mobility/traci/VehicleSignal.h \
+  ../../veins/src/veins/base/utils/EnumBitset.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCICommandInterface.h \
+  ../../veins/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h \
+  veins_inet/veins_inet.h
+
+veins_inet/VeinsInetTransparentMobility.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/mobility/base/MobilityBase.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/ModuleAccess.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/INETDefs.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/precompiled_release.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/precompiled.h:
+
+/omnetpp-6.0/include/omnetpp.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/opp_defines.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/features.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/Compat.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/InitStages.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/InitStageRegistry.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/StringFormat.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/geometry/common/CanvasProjection.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/geometry/common/RotationMatrix.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/geometry/common/Coord.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/INETMath.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/geometry/common/EulerAngles.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/Units.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/geometry/common/Quaternion.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/mobility/contract/IMobility.h:
+
+/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/ModuleRefByPar.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIMobility.h:
+
+../../veins/src/veins/base/modules/BaseMobility.h:
+
+../../veins/src/veins/veins.h:
+
+../../veins/src/veins/base/modules/BatteryAccess.h:
+
+../../veins/src/veins/base/modules/BaseModule.h:
+
+../../veins/src/veins/base/utils/HostState.h:
+
+../../veins/src/veins/base/modules/BaseBattery.h:
+
+../../veins/src/veins/base/utils/Coord.h:
+
+../../veins/src/veins/base/utils/FWMath.h:
+
+../../veins/src/veins/base/utils/Move.h:
+
+../../veins/src/veins/base/modules/BaseWorldUtility.h:
+
+../../veins/src/veins/base/utils/FindModule.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIScenarioManager.h:
+
+../../veins/src/veins/base/connectionManager/BaseConnectionManager.h:
+
+../../veins/src/veins/base/utils/AntennaPosition.h:
+
+../../veins/src/veins/base/connectionManager/NicEntry.h:
+
+../../veins/src/veins/base/utils/Heading.h:
+
+../../veins/src/veins/modules/utility/HasLogProxy.h:
+
+../../veins/src/veins/modules/obstacle/ObstacleControl.h:
+
+../../veins/src/veins/modules/obstacle/Obstacle.h:
+
+../../veins/src/veins/modules/world/annotations/AnnotationManager.h:
+
+../../veins/src/veins/modules/utility/BBoxLookup.h:
+
+../../veins/src/veins/modules/obstacle/VehicleObstacleControl.h:
+
+../../veins/src/veins/modules/obstacle/MobileHostObstacle.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIBuffer.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIConstants.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIColor.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIConnection.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCICoord.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h:
+
+../../veins/src/veins/modules/mobility/traci/VehicleSignal.h:
+
+../../veins/src/veins/base/utils/EnumBitset.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCICommandInterface.h:
+
+../../veins/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h:
+
+veins_inet/veins_inet.h:
