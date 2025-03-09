d94147838ad85d0b51677582c73d6c5798e87c1d
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-03-09 17:35:11 -0300

    Removendo a pasta 'out'

--- VANETTutorials/out/clang-release/src/veins_inet/VeinsInetSampleMessage_m.h.d
+++
@@ -1,9 +0,0 @@
-veins_inet/VeinsInetSampleMessage_m.cc veins_inet/VeinsInetSampleMessage_m.h : \
-	veins_inet/VeinsInetSampleMessage.msg \
-	/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/INETDefs.msg \
-	/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/Units.msg \
-	/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/packet/chunk/Chunk.msg
-veins_inet/VeinsInetSampleMessage.msg:
-/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/INETDefs.msg:
-/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/Units.msg:
-/home/src/Aprendendo_VANETs/02_-_How_to_Create_a_New_OMNETpp_Project_That_Works_with_INET_and_Veins/workspace/inet4.5/src/inet/common/packet/chunk/Chunk.msg:
